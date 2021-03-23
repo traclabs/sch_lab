d1f26414ae68c5b3c99edbb5da9ea9915addd81b
Seth Gee <seth@traclabs.com>
2021-03-23 16:41:44 -0500

    updated the TL fork to use the ros app for hello world

--- fsw/tables/sch_lab_table.c
+++ fsw/tables/sch_lab_table.c
@@ -31,6 +31,7 @@
 #include "to_lab_msgids.h"
 
 #include "sample_app_msgids.h"
+#include "ros_app_msgids.h"
 
 #if 0
 #include "sc_msgids.h"
@@ -58,6 +59,9 @@ SCH_LAB_ScheduleTable_t SCH_TBL_Structure = {.Config = {
                                                  {CFE_SB_MSGID_WRAP_VALUE(CI_LAB_SEND_HK_MID), 4},
                                                  {CFE_SB_MSGID_WRAP_VALUE(TO_LAB_SEND_HK_MID), 4},
                                                  {CFE_SB_MSGID_WRAP_VALUE(SAMPLE_APP_SEND_HK_MID), 4},
+                                                 //{CFE_SB_MSGID_WRAP_VALUE(SAMPLE_APP_CMD_MID), 4},
+                                                 {CFE_SB_MSGID_WRAP_VALUE(ROS_APP_SEND_HK_MID), 4},
+                                                 {CFE_SB_MSGID_WRAP_VALUE(ROS_APP_CMD_MID), 4},
 #if 0
                 {CFE_SB_MSGID_WRAP_VALUE(SC_SEND_HK_MID),       4},
                 {CFE_SB_MSGID_WRAP_VALUE(SC_1HZ_WAKEUP_MID),    1},  /* Example of a 1hz packet */
